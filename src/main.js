6c354db85c4ddfbcc4c0a38d0eb79d180ad9e71f
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-07-15 15:46:55 +0200

    install dependiencies

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Opisany w dokumentacji
+import iziToast from 'izitoast';
+// Opcjonalny import stylów
+import 'izitoast/dist/css/iziToast.min.css';
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Opcjonalny import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
